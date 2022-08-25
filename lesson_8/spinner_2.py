349ddbd7c3e709caeb52caf9625f6b285bacb4a0
Anastasiia Babenko <radiowavezes@gmail.com>
2022-08-25 11:22:14 +0300

    modified spinne2

--- lesson_8/spinner_2.py
+++ lesson_8/spinner_2.py
@@ -1,13 +1,20 @@
 import time
 
 
-def spinner(seconds, some_list):
-    for i in range(seconds):
-        for j in range(len(some_list)):
-            time.sleep(0.33)
-            print(some_list[j], end='')
+def spinner(seconds, some_list, now_time):
+    while True:
+        current_time = time.time()
+        elapsed_time = current_time - now_time
+        if elapsed_time > seconds:
+            break
+        else:
+            for i in range(seconds):
+                for j in range(len(some_list)):
+                    time.sleep(0.33)
+                    print(some_list[j], end='')
         
 
+start_time = time.time()
 list_of_symb = [
         '\\\\\\\r\r\r', 
         '|||\r\r\r', 
@@ -15,4 +22,4 @@ list_of_symb = [
         '---\r\r\r']
 
 
-spinner(int(input('How long do you want to watch this??? Seconds --> ')), list_of_symb)
+spinner(int(input('How long do you want to watch this??? Seconds --> ')), list_of_symb, start_time)
