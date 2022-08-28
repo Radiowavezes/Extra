9e6465a082321886df7d7f1f7ac596c2d276b888
Radiowavezes <radiowavezes@gmail.com>
2022-08-28 09:43:11 +0300

    -

--- lesson_10/lesson9_task2.py
+++ lesson_10/lesson9_task2.py
@@ -2,8 +2,7 @@ def strange_math(a, b):
     try:
         return int(a)**2 / int(b)
     except (ValueError, ZeroDivisionError):
-        print('Give me an integer and don\'t try to devide by zero!')
-        raise
+        return 'Give me an integer and don\'t try to devide by zero!'
 
 
 print(strange_math(input('What to devide: '), input('...by: ')))
