d8505d51d40dcebaecb38ab3cf8d06063fe58805
Anastasiia Babenko <radiowavezes@gmail.com>
2022-10-05 21:18:01 +0300

    added recursive function for valid parentheses from codewars

--- lesson_24/recursive_functions.py
+++ lesson_24/recursive_functions.py
@@ -118,3 +118,27 @@ print(sum_of_digits('26'))
 print(sum_of_digits('263'))
 print(sum_of_digits('123456'))
 print(sum_of_digits('test'))
+
+
+def valid_parentheses(strng):
+    '''
+    task from codewars
+    '''
+    
+    new_strng = ''
+    for symbol in strng:
+        if symbol in '()':
+            new_strng += symbol
+            
+    def recursive_parentheses(new_strng):
+        if len(new_strng) == 0:
+            return True
+        else:
+            if '()' in new_strng:
+                return recursive_parentheses(new_strng.replace('()', ''))
+            return False
+        
+    return recursive_parentheses(new_strng)
+
+
+print(valid_parentheses("hi(hi)()"))
