b143d66b898fbd6be4d2e00256f381cf3af04691
Anastasiia Babenko <radiowavezes@gmail.com>
2022-10-20 13:55:07 +0300

    -

--- lesson_19/lms_task1.py
+++
@@ -1,8 +0,0 @@
-def with_index(iterable, start=0):
-    current = start
-    for item in iterable:
-        yield current, item
-        current += 1
-
-my_list = ['a', 'b', 'C', 'De', 'x', 'P']
-print(*with_index(my_list, 2))
--- lesson_19/lms_task2.py
+++
@@ -1,8 +0,0 @@
-def in_range(start, stop, step=1):
-    current = start
-    while current < stop:
-        yield current
-        current += step
-
-
-print(*in_range(2, 10, 3))
