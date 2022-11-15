881621d283f6602cecd9c4b3a73c70ff5e272333
Anastasiia Babenko <radiowavezes@gmail.com>
2022-11-15 12:53:24 +0200

    changed the logic of delivering with encryption and decryption on both sides. The passkey is'not obtained by the server now but hardcoded on both sides

--- 36_Network_Programming/task2_client_TCP.py
+++ 36_Network_Programming/task2_client_TCP.py
@@ -1,11 +1,22 @@
 from socket import *
-key = (input('Input the key for message to decrypt 1-9: '))
-message = 'WrgdB lv d jrrg gdB!'
+
+passkey = 3
+message = input('Please, input your message here: ')
+
+def encryption(recieved_message, key):
+    response_message = ''
+    caesar_al = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    for symbol in recieved_message:
+        if symbol.isalpha():
+            new_index = (caesar_al.find(symbol) + key) % 52
+            symbol = caesar_al[new_index]
+        response_message += symbol
+    return response_message
 
 with socket(AF_INET, SOCK_STREAM) as s:
     s.connect(('127.0.0.1', 64623))
-    s.sendall(bytes(key, encoding='utf-8'))
-    s.sendall(bytes(message, encoding='utf-8'))
+    s.sendall(bytes(encryption(message, passkey), encoding='utf-8'))
     data = s.recv(1024)
 
 print(f'Received {data!r}')
+ 
--- 36_Network_Programming/task2_server_TCP.py
+++ 36_Network_Programming/task2_server_TCP.py
@@ -1,11 +1,11 @@
 from socket import *
 
+passkey = 3
 
-def decryption(recieved_message):
-    key = int(recieved_message[0])
+def decryption(recieved_message, key):
     response_message = ''
     caesar_al = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    for symbol in recieved_message[1:]:
+    for symbol in recieved_message:
         if symbol.isalpha():
             new_index = (caesar_al.find(symbol) - key) % 52
             symbol = caesar_al[new_index]
@@ -21,7 +21,7 @@ with socket(AF_INET, SOCK_STREAM) as s:
         print(f'Connected by {address}')
         while True:
             data = connection.recv(1024)
-            response_message = decryption(data.decode('utf-8'))
+            response_message = decryption(data.decode('utf-8'), passkey)
             if not data:
                 break
             connection.sendall(bytes(response_message, encoding='utf-8'))
