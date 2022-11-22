7cb5ba23a66f02d91144c9baf21259ad94beb9fe
Anastasiia Babenko <radiowavezes@gmail.com>
2022-11-22 15:08:02 +0200

    -

--- 38_Threads/task2_multi_server.py
+++ 38_Threads/task2_multi_server.py
@@ -1,22 +1,35 @@
 from socket import *
+from _thread import *
 import threading
+import time
+
+print_lock = threading.Lock()
+
+def threaded(connection, sock):
+    while True:
+        data = connection.recv(1024)
+        if not data:
+            print('Goodbye')
+            print_lock.release()
+            break
+        if data.decode().lower() == 'stop':
+            time.sleep(1)
+            sock.close()
+            break
+        print(f'Server recieved "{data.decode()}"')
+        connection.send(data)
+    connection.close()
 
 
-def serving():
-    with socket(AF_INET, SOCK_STREAM) as s:
-        s.bind(('127.0.0.1', 64623))
-        s.listen()
+s = socket(AF_INET, SOCK_STREAM)
+s.bind(('127.0.0.1', 64623))
+s.listen(5)
+
+try:
+    while True:
         connection, address = s.accept()
-        with connection:
-            print(f'Connected by {address}')
-            while True:
-                data = connection.recv(1024)
-                if not data:
-                    print(threading.current_thread().name, 'Disconnected')
-                    break
-                connection.sendall(data)
-
-connection1 = threading.Thread(name='Server1', target=serving)
-
-connection1.start()
-            
+        print_lock.acquire()
+        print(f'Connected to: {address[0]} : {address[1]}')
+        start_new_thread(threaded, (connection, s))
+except OSError:
+    print('Closed')
