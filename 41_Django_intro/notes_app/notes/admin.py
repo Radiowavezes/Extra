9381055059d7e82ec424944e8961eadbb011ee6b
Anastasiia Babenko <radiowavezes@gmail.com>
2022-12-03 23:04:23 +0200

    -

--- 41_Django_intro/notes_app/notes/admin.py
+++ 41_Django_intro/notes_app/notes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Notes
+from .models import Category
 
-# Register your models here.
+class NotesAdmin(admin.ModelAdmin):
+    list_display = ('text', 'title', 'reminder', 'pub_date')
+
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ('headline', 'all_notes')
+
+admin.site.register(Notes, NotesAdmin)
+admin.site.register(Category)
